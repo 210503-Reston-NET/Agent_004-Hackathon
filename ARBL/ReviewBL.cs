5a150fde36102c0c1e66d3bf73b1fd83552dff34
cesar-2020 <83565973+cesar-2020@users.noreply.github.com>
2021-05-29 21:52:01 -0500

    Add files via upload

---
+++ ARBL/ReviewBL.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using ARDL;
+using ARModels;
+
+namespace ARBL
+{
+    public class ReviewBL : IReviewBL
+    {
+        private IRepository _repo;
+        public ReviewBL(IRepository repo)
+        {
+            _repo = repo;
+        }
+        public Review AddReview(Associate Associate, Review review)
+        {
+     
+            _repo.AddReview(Associate, review);
+            return review;
+        }
+
+        public Tuple<List<Review>, int> GetReviews(Associate Associate)
+        {
+          
+            List<Review> AssociateReviews = _repo.GetReviews(Associate);
+            int averageRating = 0;
+            AssociateReviews.ForEach(review => averageRating += review.Rating);
+            int revs = AssociateReviews.Count;
+            if(revs != 0)
+            {
+                averageRating = averageRating / AssociateReviews.Count;
+            }
+            return new Tuple<List<Review>, int>(AssociateReviews, averageRating);
+        }
+    }
+}
